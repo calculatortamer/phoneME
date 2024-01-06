714b9f36879cf3995648e1b81b62ed239bb814c0
calculatortamer <87210024+calculatortamer@users.noreply.github.com>
2024-01-06 20:01:56 +0100

    update build script to zip both cldc and midp classes

--- build.sh
+++ build.sh
@@ -7,5 +7,10 @@ make -C javacall/configuration/stubs/linux/
 CFLAGS="-m32" make -C pcsl
 #build cldc vm
 make -C cldc/build/javacall_i386_gcc/
-#build midp, note : ROMIZING will fail
+#build midp, note : runMidlet will fail (but its ok)
 make -C midp/build/javacall/
+#merge cldc and midp classes
+mkdir $BUILD_OUTPUT_DIR/mergedclasses
+cp -r $MIDP_OUTPUT_DIR/classes/* $CLDC_OUTPUT_DIR/classes/* $BUILD_OUTPUT_DIR/mergedclasses
+cd $BUILD_OUTPUT_DIR/mergedclasses
+zip -9 -r cldc_midp_classes.zip .
